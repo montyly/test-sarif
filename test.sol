eb493589ae52169ca8a3daec3fa62dfaf0f3a332
Feist Josselin <josselin@trailofbits.com>
2022-02-10 10:57:21 +0100

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,9 +1,13 @@
-contract A{
+contract Test{
+    mapping(address => uint) balances;
+    
+    function deposit() external payable{
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdraw() external{
+        msg.sender.call{value: balances[msg.sender]}("");
+         balances[msg.sender] = 0;
+    }
 
-    uint test;
-}
-
-contract B is A{
-
-    uint test;
 }
